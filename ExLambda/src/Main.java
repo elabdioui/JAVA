d6b763b588668b223044f567d7b9ab0042a7cf48
Haitham El Abdioui <152732703+elabdioui@users.noreply.github.com>
2025-01-20 22:46:22 +0100

    Update Main.java

--- ExLambda/src/Main.java
+++ ExLambda/src/Main.java
@@ -3,6 +3,8 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//expression lambda
+
 public class Main {
     public static void main(String[] args) {
 
